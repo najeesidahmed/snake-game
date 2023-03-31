9768eaa97b7f9a824f48544c0a91cb28dab3f997
najee sidahmed <najeekamil@msn.com>
2023-03-31 01:11:40 +0100

    add score functionality, and control buttons for smaller screens

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const playBoard = document.querySelector(".play-board");
+const scoreElement = document.querySelector(".score");
+const highScoreElement = document.querySelector(".high-score");
 
 let gameOver = false;
 let foodX, foodY;
@@ -6,6 +8,13 @@ let snakeX = 5, snakeY = 10;
 let snakeBody = [];
 let velocityX = 0, velocityY = 0;
 let setIntervalId;
+let score = 0;
+const controls = document.querySelectorAll(".controls i");
+
+// get high score from local storage
+let highScore = localStorage.getItem("high-score") || 0;
+highScoreElement.innerText = `High Score: ${highScore}`;
+
 
 const changeFoodPosition = () => {
     // pass random 0-30 value as food position
@@ -27,6 +36,12 @@ const initGame = () => {
     if(snakeX === foodX && snakeY === foodY){
         changeFoodPosition();
         snakeBody.push([foodX, foodY]); // push food position to snake body array
+        score++;
+
+        highScore = score >= highScore ? score : highScore;
+        localStorage.setItem("high-score", highScore);
+        scoreElement.innerText = `Score: ${score}`;
+        highScoreElement.innerText = `High Score: ${highScore}`;
     }
 
     for (let i = snakeBody.length -1; i > 0; i--){
@@ -74,6 +89,11 @@ const changeDirection = (e) => {
     }
 
 }
+
+controls.forEach(key => {
+    // call changeDirection on each key press
+    key.addEventListener("click", () => changeDirection({ key: key.dataset.key }));
+})
 
 changeFoodPosition();
 setIntervalId = setInterval(initGame, 125);
